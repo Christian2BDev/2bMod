7efb141c3e965cc16a02384a1c49bc65c909d498
Christian2BDev <christiantenbrinke@hotmail.com>
2023-10-28 16:23:55 +0200

    Initial commit

---
+++ src/main/java/com/heckvision/tbmod/TbModClient.java
@@ -0,0 +1,10 @@
+package com.heckvision.tbmod;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class TbModClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
